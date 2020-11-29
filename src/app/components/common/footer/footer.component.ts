52f7ecfd1ec4e7b333c2f976e7b573fb4fcd5a21
Matt Gennings <mattguy19@gmail.com>
2020-11-29 14:19:10 -0500

    content changes

--- src/app/components/common/footer/footer.component.ts
+++ src/app/components/common/footer/footer.component.ts
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent implements OnInit {
+  public today = new Date();
+  public currentYear = this.today.getFullYear()
 
   constructor() { }
 
